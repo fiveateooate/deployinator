0a5a0f1a30e6c18799716a7ef43d10132696d748
fiveateooate <fiveateooate@users.noreply.github.com>
2019-07-16 13:25:16 -0600

    makerandom not caps

--- internal/common/sharedfuncs.go
+++ internal/common/sharedfuncs.go
@@ -13,7 +13,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterBytes = "abcdefghijklmnopqrstuvwxyz0123456789"
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
